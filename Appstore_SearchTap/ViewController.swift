b619da9b4d2e9e6ae85a1b552a89f73cdfd7a5fe
예니 <cho970416@gmail.com>
2023-03-18 22:43:13 +0900

    Initial Commit

---
+++ Appstore_SearchTap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appstore_SearchTap
+//
+//  Created by 조예은 on 2023/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Appstore_SearchTapUITests/Appstore_SearchTapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Appstore_SearchTapUITestsLaunchTests.swift
+//  Appstore_SearchTapUITests
+//
+//  Created by 조예은 on 2023/03/18.
+//
+
+import XCTest
+
+final class Appstore_SearchTapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
